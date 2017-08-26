de395f17bd07fb8d98b8648f2c9b597b4057ccff
zabador <zabador2000@gmail.com>
2017-08-25 23:51:43 -0400

    Convert java to kotlin

---
+++ app/src/main/java/net/zabador/money/service/NotificationServiceAgain.kt
@@ -0,0 +1,28 @@
+package net.zabador.money.service
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+/**
+ * Created by Skye Schneider. The mad king of Domino's
+ * in the year of our Lord 8/25/17.
+ */
+
+class NotificationServiceAgain : NotificationListenerService() {
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        Log.d(TAG, "New Notification Posted")
+
+        val extras = sbn.notification.extras
+
+        if (extras != null) {
+        }
+    }
+
+    companion object {
+
+        private val TAG = NotificationServiceAgain::class.java.simpleName
+    }
+
+}
